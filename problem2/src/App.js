4d302c068bfaf888b4d0a3838d72037a2bb46a6f
Vivek.Abhale <vivek.abhale48@gmail.com>
2024-10-04 20:50:30 +0530

    updated the pagination

--- problem2/src/App.js
+++ problem2/src/App.js
@@ -4,6 +4,13 @@ import './App.css';
 function App() {
   const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
+  const [stopLoop, setStopLoop] = useState(3)
+  const [startLoop, setStartLoop] = useState(0);
+
+  let numberOfPostPerPage = 10;
+  let numberOfPages = data.length / numberOfPostPerPage;
+  let indexOfFirstPostOnPage = currentPage * numberOfPostPerPage;
+  let indexOfLastPostOnPage = indexOfFirstPostOnPage + numberOfPostPerPage;
 
   useEffect(()=> {
     fetchData();
@@ -15,10 +22,25 @@ function App() {
     setData(result);
   }
 
-  let numberOfPostPerPage = 10;
-  let numberOfPages = data.length / numberOfPostPerPage;
-  let indexOfFirstPostOnPage = currentPage * numberOfPostPerPage;
-  let indexOfLastPostOnPage = indexOfFirstPostOnPage + numberOfPostPerPage;
+  function handleNext() {
+    if(currentPage === stopLoop - 2) {
+      setStopLoop(stopLoop+1)
+      setStartLoop(startLoop+1)
+    }
+    setCurrentPage(currentPage + 1)
+  }
+
+  function handleOnPageNumberClick(index) {
+    setCurrentPage(index)
+    if(currentPage === stopLoop - 2) {
+      setStopLoop(stopLoop+1)
+      setStartLoop(startLoop+1)
+    }
+  }
+
+  function handlePrevious() {
+    setCurrentPage(currentPage - 1)
+  }
 
   const displayData = data.slice(indexOfFirstPostOnPage, indexOfLastPostOnPage);
   return (
@@ -35,19 +57,30 @@ function App() {
       </ul>
 
       <div className='mt-5 flex justify-center gap-3'>
-        <button disabled={currentPage === 0} onClick={() => setCurrentPage(currentPage - 1)} className={`min-w-12 h-12 flex justify-center items-center bg-slate-200 ${currentPage === 0 ? 'cursor-not-allowed' : 'cursor-pointer'}`}>
+        <button disabled={currentPage === 0} onClick={handlePrevious} className={`hover:bg-orange-200 transition-all duration-200 min-w-12 h-12 flex justify-center items-center bg-slate-200 ${currentPage === 0 ? 'cursor-not-allowed' : 'cursor-pointer'}`}>
           Prev
         </button>
         {
-          Array.from({length: Math.ceil(numberOfPages)}, (_, index)=>{
+          Array
+          .from({length: Math.ceil(numberOfPages)}, (_, index)=> index)
+          .slice(startLoop, stopLoop)
+          .map((index) => {
             return(
-              <span onClick={() => setCurrentPage(index)} className={`w-12 h-12 flex justify-center items-center cursor-pointer ${currentPage === index ? 'bg-orange-300' : 'bg-slate-200'}`}>
-                {index + 1}
+              <span onClick={() => handleOnPageNumberClick(index)} className={`hover:bg-orange-200 transition-all duration-200 w-12 h-12 flex justify-center items-center cursor-pointer ${currentPage === index ? 'bg-orange-400' : 'bg-slate-200'}`}>
+                  {index + 1}
               </span>
             )
           })
         }
-        <button disabled={currentPage === numberOfPages - 1} onClick={() => setCurrentPage(currentPage + 1)} className={`min-w-12 h-12 flex justify-center items-center bg-slate-200 ${currentPage === numberOfPages -1 ? 'cursor-not-allowed': 'cursor-pointer'}`}>
+        {
+          currentPage !== numberOfPages - 1 && (
+            <span className='min-w-12 h-12 flex justify-center items-center bg-slate-200'>
+              ...
+            </span>
+          )
+        }
+        
+        <button disabled={currentPage === numberOfPages - 1} onClick={handleNext} className={`hover:bg-orange-200 transition-all duration-200 min-w-12 h-12 flex justify-center items-center bg-slate-200 ${currentPage === numberOfPages -1 ? 'cursor-not-allowed': 'cursor-pointer'}`}>
           Next
         </button>
       </div>
